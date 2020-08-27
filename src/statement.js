43e993a8d63e88be2b819fb87d84e337d169b3bb
hzhgit <971499086@qq.com>
2020-08-27 15:23:31 +0800

    [zach]feature:extract amountFor method

--- src/statement.js
+++ src/statement.js
@@ -5,30 +5,35 @@ function usd(amout){
     minimumFractionDigits: 2,
   }).format(amout / 100);
 }
+
+function amountFor(aPerformance,play){
+  let thisAmount = 0;
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (aPerformance.audience > 30) {
+        thisAmount += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (aPerformance.audience > 20) {
+        thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      thisAmount += 300 * aPerformance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    const thisAmount = amountFor(perf,play)
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
